6e4f0d58c79d3b72a6373ec4d5535a922eaa5a70
Balázs Lécz <leczbalazs@gmail.com>
2012-09-21 23:30:44 +0100

    Added support for repeat; optimized default timings

--- OneButton.cpp
+++ OneButton.cpp
@@ -1,14 +1,14 @@
 // -----
-// OneButton.cpp - Library for detecting button clicks, doubleclicks and long press pattern on a single button.
+// OneButton.cpp - Library for detecting clicks, double-clicks and long presses on a single button.
 // This class is implemented for use with the Arduino environment.
 // Copyright (c) by Matthias Hertel, http://www.mathertel.de
 // This work is licensed under a BSD style license. See http://www.mathertel.de/License.aspx
 // More information on: http://www.mathertel.de/Arduino
 // -----
-// Connect the button to pin A1 (MenuPin)
 // 02.10.2010 created by Matthias Hertel
 // 21.04.2011 support of active LOW and active HIGH button signal input.
 // 01.12.2011 include file changed to work with the Arduino 1.0 environment
+// 21.09.2012 Balázs Lécz: added support for repeat; optimized default timings
 // -----
 
 #include "OneButton.h"
@@ -17,12 +17,13 @@
 
 OneButton::OneButton(int pin, int activeLow)
 {
-  pinMode(pin, INPUT);      // sets the MenuPin as input
+  pinMode(pin, INPUT); // sets pin as input
   _pin = pin;
 
-  _clickTicks = 600;        // number of millisec that have to pass by before a click is detected.
-  _pressTicks = 1000;       // number of millisec that have to pass by before a lonn button press is detected.
- 
+  _clickTicks = 80; // timeout for detecting a single click [milliseconds]
+  _pressTicks = 750; // timeout for detecting a long-press [milliseconds]
+  _repeatTicks = 60; // delay between two repetitions [milliseconds]
+
   _state = 0; // starting with state 0: waiting for button to be pressed
 
   if (activeLow) {
@@ -30,12 +31,12 @@ OneButton::OneButton(int pin, int activeLow)
     _buttonReleased = HIGH; // notPressed
     _buttonPressed = LOW;
     digitalWrite(pin, HIGH);   // turn on pulldown resistor
-
-  } else {
-    // button connects VCC to the pin when pressed.
+  } 
+  else {
+    // button connects Vcc to the pin when pressed.
     _buttonReleased = LOW;
     _buttonPressed = HIGH;
-  } // if
+  }
 
 } // OneButton
 
@@ -52,6 +53,14 @@ void OneButton::setPressTicks(int ticks) {
 } // setPressTicks
 
 
+void OneButton::setRepeatTicks(int ticks) {
+  _repeatTicks = ticks;
+}
+
+void OneButton::setRepeat(boolean repeat) {
+  _repeat = repeat;
+}
+
 // save function for click event
 void OneButton::attachClick(callbackFunction newFunction)
 {
@@ -67,9 +76,10 @@ void OneButton::attachDoubleClick(callbackFunction newFunction)
 
 
 // save function for press event
-void OneButton::attachPress(callbackFunction newFunction)
+void OneButton::attachPress(callbackFunction newFunction, boolean repeat)
 {
   _pressFunc = newFunction;
+  _repeat = repeat;
 } // attachPress
 
 
@@ -91,9 +101,9 @@ void OneButton::tick(void)
       _state = 2; // step to state 2
 
     } else if ((buttonLevel == _buttonPressed) && (now > _startTime + _pressTicks)) {
+      _startTime = now;
       if (_pressFunc) _pressFunc();
       _state = 6; // step to state 6
-      
     } else {
       // wait. Stay in this state.
     } // if
@@ -118,11 +128,15 @@ void OneButton::tick(void)
   } else if (_state == 6) { // waiting for menu pin being release after long press.
     if (buttonLevel == _buttonReleased) {
       _state = 0; // restart.
-    } // if  
+    } else if (_repeat && (now > _startTime + _repeatTicks)) {
+      _startTime = now;
+      if (_pressFunc) _pressFunc();
+    }  
 
   } // if  
 } // OneButton.tick()
 
 
 // end.
+
 
--- OneButton.h
+++ OneButton.h
@@ -1,5 +1,5 @@
 // -----
-// OneButton.h - Library for detecting button clicks, doubleclicks and long press pattern on a single button.
+// OneButton.h - Library for detecting clicks, double-clicks and long presses on a single button.
 // This class is implemented for use with the Arduino environment.
 // Copyright (c) by Matthias Hertel, http://www.mathertel.de
 // This work is licensed under a BSD style license. See http://www.mathertel.de/License.aspx
@@ -8,6 +8,7 @@
 // 02.10.2010 created by Matthias Hertel
 // 21.04.2011 transformed into a library
 // 01.12.2011 include file changed to work with the Arduino 1.0 environment
+// 21.09.2012 Balázs Lécz: added support for repeat; optimized default timings; general cleanups
 // -----
 
 #ifndef OneButton_h
@@ -27,7 +28,7 @@ class OneButton
 public:
   // ----- Constructor -----
   OneButton(int pin, int active);
-  
+
   // ----- Set runtime parameters -----
 
   // set # millisec after single click is assumed.
@@ -35,11 +36,13 @@ public:
 
   // set # millisec after press is assumed.
   void setPressTicks(int ticks);
+  void setRepeatTicks(int ticks);
+  void setRepeat(boolean repeat);
 
   // attach functions that will be called when button was pressed in th especified way.
   void attachClick(callbackFunction newFunction);
   void attachDoubleClick(callbackFunction newFunction);
-  void attachPress(callbackFunction newFunction);
+  void attachPress(callbackFunction newFunction, boolean repeat);
 
   // ----- State machine functions -----
 
@@ -49,10 +52,12 @@ public:
 private:
   int _pin;        // hardware pin number. 
   int _clickTicks; // number of ticks that have to pass by before a click is detected
-  int _pressTicks; // number of ticks that have to pass by before a lonn button press is detected
-
+  int _pressTicks; // number of ticks that have to pass by before a long button press is detected
+  int _repeatTicks;
+  
   int _buttonReleased;
   int _buttonPressed;
+  boolean _repeat;
 
 
   // These variables will hold functions acting as event source.
@@ -67,5 +72,6 @@ private:
 };
 
 #endif
+
 
 
